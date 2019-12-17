2a6bc497b1d0eee93aca5e2c21c142bda70ee849
nagalab <no email>
2019-12-17 14:41:37 +0900

    change gan→gen

--- gen1.py
+++ gen1.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from chainer import training, datasets, iterators, optimizers, cuda
+from chainer import training, datasets, iterators, optimizers ,reporter, cuda
 from chainer.training import extensions
 from chainer.datasets import TupleDataset
 import numpy as np
 import os
 import math
-import random
 from numpy import random
 from PIL import Image
-import codecs
 
+batch_size = 10			# バッチサイズ10
 uses_device = 0			# GPU#0を使用
 
 # GPU使用時とCPU使用時でデータ形式が変わる
@@ -46,27 +44,6 @@ def adain(content_feature, style_feature):
     normalized_feat = (content_feature - content_mean) / content_std
 
     return normalized_feat * style_std + style_mean
-
-class VGG(chainer.Chain):
-
-	def __init__(self):
-		w = chainer.initializers.GlorotUniform()
-		super(VGG, self).__init__()
-		with self.init_scope():
-			self.base = L.VGG16Layers()
-
-			self.c0=L.Convolution2D(512, 512, 4, 2, 1, initialW=w)
-			self.c1=L.Convolution2D(512, 512, 3, 1, 1, initialW=w)
-
-			self.bnc0=L.BatchNormalization(512)
-			self.bnc1=L.BatchNormalization(512)
-
-	def __call__(self, x):
-		h = F.relu(self.base(x, layers=['conv4_3'])['conv4_3'])
-		h = F.relu(self.bnc0(self.c0(h)))
-		h = F.relu(self.bnc1(self.c1(h)))
-
-		return h
 
 class CBR(chainer.Chain):
 
@@ -136,16 +113,22 @@ class Upsamp(chainer.Chain):
 		return h
 
 # ベクトルから画像を生成するNN
-class DCGAN_Generator_NN(chainer.Chain):
+class Generator_NN(chainer.Chain):
 
 	def __init__(self,base = 32):
 		# 重みデータの初期値を指定する
 		w = chainer.initializers.GlorotUniform()
 		# 全ての層を定義する
-		super(DCGAN_Generator_NN, self).__init__()
+		super(Generator_NN, self).__init__()
 		with self.init_scope():
+
 			#ヒント画像
-			self.vgg16 = VGG()
+			self.x2c0 = L.Convolution2D(3, base, 3, 1, 1, initialW=w)
+			self.x2bnc0 = L.BatchNormalization(base)
+			self.x2cbr0 = CBR(base, base*2)
+			self.x2cbr1 = CBR(base*2, base*4)
+			self.x2cbr2 = CBR(base*4, base*8)
+			self.x2cbr3 = CBR(base*8, base*16)
 
 			# Input layer
 			self.c0 = L.Convolution2D(3, base, 3, 1, 1, initialW=w)
@@ -156,22 +139,29 @@ class DCGAN_Generator_NN(chainer.Chain):
 			self.cbr1 = CBR(base*2, base*4)
 			self.cbr2 = CBR(base*4, base*8)
 			self.cbr3 = CBR(base*8, base*16)
+			self.cbr4 = CBR(base*32, base*16)
 			self.res0 = ResBlock(base*16, base*16)
 			self.res1 = ResBlock(base*16, base*16)
 			self.ad0 = AdainResBlock(base*16, base*16)
 			self.ad1 = AdainResBlock(base*16, base*16)
 			self.ad2 = AdainResBlock(base*16, base*16)
 			self.ad3 = AdainResBlock(base*16, base*16)
-			self.up0 = Upsamp(base*16, base*8)
-			self.up1 = Upsamp(base*16, base*4)
-			self.up2 = Upsamp(base*8, base*2)
-			self.up3 = Upsamp(base*4, base)
+			self.up0 = Upsamp(base*16, base*16)
+			self.up1 = Upsamp(base*16, base*8)
+			self.up2 = Upsamp(base*16, base*4)
+			self.up3 = Upsamp(base*8, base*2)
+			self.up4 = Upsamp(base*4, base)
 
 			# Output layer
 			self.c1 = L.Convolution2D(base*2, 3, 3, 1, 1, initialW=w)
 			
 	def __call__(self, x1,x2):
-		v16 = self.vgg16(x2)
+
+		x2e0 = F.relu(self.x2bnc0(self.x2c0(x2)))
+		x2e1 = self.x2cbr0(x2e0)
+		x2e2 = self.x2cbr1(x2e1)
+		x2e3 = self.x2cbr2(x2e2)
+		x2e4 = self.x2cbr3(x2e3)
 
 		#U-Net
 		e0 = F.relu(self.bnc0(self.c0(x1)))
@@ -179,69 +169,61 @@ class DCGAN_Generator_NN(chainer.Chain):
 		e2 = self.cbr1(e1)
 		e3 = self.cbr2(e2)
 		e4 = self.cbr3(e3)
-		r0 = self.res0(e4)
+		e5 = self.cbr4(F.concat([e4, x2e4]))
+		r0 = self.res0(e5)
 		r1 = self.res1(r0)
-		a0 = self.ad0(r1,v16)
-		a1 = self.ad1(a0,v16)
-		a2 = self.ad2(a1,v16)
-		a3 = self.ad3(a2,v16)
+		a0 = self.ad0(r1,x2e4)
+		a1 = self.ad1(a0,x2e4)
+		a2 = self.ad2(a1,x2e4)
+		a3 = self.ad3(a2,x2e4)
 		d0 = self.up0(a3)
-		d1 = self.up1(F.concat([d0, e3]))
-		d2 = self.up2(F.concat([d1, e2]))
-		d3 = self.up3(F.concat([d2, e1]))
-		d4 = F.sigmoid(self.c1(F.concat([d3,e0])))
+		d1 = self.up1(d0)
+		d2 = self.up2(F.concat([d1, e3]))
+		d3 = self.up3(F.concat([d2, e2]))
+		d4 = self.up4(F.concat([d3, e1]))
+		d5 = F.sigmoid(self.c1(F.concat([d4,e0])))
 		
-		return d4	# 結果を返すのみ
+		return d5	# 結果を返すのみ
 
-# 画像を確認するNN
-class DCGAN_Discriminator_NN(chainer.Chain):
+# カスタムUpdaterのクラス
+class Updater(training.StandardUpdater):
 
-	def __init__(self):
-		# 重みデータの初期値を指定する
-		w = chainer.initializers.GlorotUniform()
-		super(DCGAN_Discriminator_NN, self).__init__()
-		# 全ての層を定義する
-		with self.init_scope():
-			self.c0_0 = L.Convolution2D(3, 8, 3, 1, 1, initialW=w)
-			self.c0_1 = L.Convolution2D(8, 16, 4, 2, 1, initialW=w)
-			self.c1_0 = L.Convolution2D(16, 16, 3, 1, 1, initialW=w)
-			self.c1_1 = L.Convolution2D(16, 32, 4, 2, 1, initialW=w)
-			self.c2_0 = L.Convolution2D(32, 32, 3, 1, 1, initialW=w)
-			self.c2_1 = L.Convolution2D(32, 64, 4, 2, 1, initialW=w)
-			self.c3_0 = L.Convolution2D(64, 64, 3, 1, 1, initialW=w)
-			self.l4 = L.Linear(128 * 128, 1, initialW=w)
-			self.bn0_1 = L.BatchNormalization(16, use_gamma=False)
-			self.bn1_0 = L.BatchNormalization(16, use_gamma=False)
-			self.bn1_1 = L.BatchNormalization(32, use_gamma=False)
-			self.bn2_0 = L.BatchNormalization(32, use_gamma=False)
-			self.bn2_1 = L.BatchNormalization(64, use_gamma=False)
-			self.bn3_0 = L.BatchNormalization(64, use_gamma=False)
+	def __init__(self, train_iter, optimizer, device):
+		super(Updater, self).__init__(
+			train_iter,
+			optimizer,
+			device=device
+		)
 
-	def __call__(self, x):
-		h = F.leaky_relu(self.c0_0(x))
-		h = F.dropout(F.leaky_relu(self.bn0_1(self.c0_1(h))),ratio=0.2)
-		h = F.dropout(F.leaky_relu(self.bn1_0(self.c1_0(h))),ratio=0.2)
-		h = F.dropout(F.leaky_relu(self.bn1_1(self.c1_1(h))),ratio=0.2)
-		h = F.dropout(F.leaky_relu(self.bn2_0(self.c2_0(h))),ratio=0.2)
-		h = F.dropout(F.leaky_relu(self.bn2_1(self.c2_1(h))),ratio=0.2)
-		h = F.dropout(F.leaky_relu(self.bn3_0(self.c3_0(h))),ratio=0.2)
-		return self.l4(h)	# 結果を返すのみ
+	def loss(self,gen,t):
+		loss = F.mean_absolute_error(gen, t)
+		reporter.report({'loss':loss})
 
+		return loss
+
+	def update_core(self):
+		# Iteratorからバッチ分のデータを取得
+		batch = self.get_iterator('main').next()
+		src = self.converter(batch, self.device)
+		
+		# Optimizerを取得
+		optimizer_gen = self.get_optimizer('opt_gen')
+		# ニューラルネットワークのモデルを取得
+		gen = optimizer_gen.target
+
+		optimizer_gen.update(self.loss, gen(src[0],src[1]), src[1])
+		
 # ニューラルネットワークを作成
-model = DCGAN_Generator_NN()
+model_gen = Generator_NN()
 
 if uses_device >= 0:
 	# GPUを使う
 	chainer.cuda.get_device_from_id(0).use()
 	chainer.cuda.check_cuda_available()
 	# GPU用データ形式に変換
-	model.to_gpu()
+	model_gen.to_gpu()
 
-# 学習結果を読み込む
-chainer.serializers.load_hdf5( 'gan-gen-30.hdf5', model )
-
-
-# 画像を生成する
+#chainer.serializers.load_hdf5( 'gen-50.hdf5', model_gen )
 
 listdataset1 = []
 listdataset2 = []
@@ -250,7 +232,6 @@ fs = os.listdir('/home/nagalab/soutarou/colorrization/images')
 fs.sort()
 
 for fn in fs:
-	#print(fn)
 	# 画像を読み込んで128×128ピクセルにリサイズ
 	img = Image.open('/home/nagalab/soutarou/colorrization/images/' + fn).convert('RGB').resize((128, 128))
 
@@ -265,24 +246,39 @@ for fn in fs:
 		hpix2= hpix2.transpose(2,0,1)
 		listdataset2.append(hpix2)
 
-#random.shuffle(listdataset2)
-
 # 配列に追加
 tupledataset1 = tuple(listdataset1)
 tupledataset2 = tuple(listdataset2)
-	
-tupledataset1 = cp.array(tupledataset1, dtype=cp.float32)
-tupledataset2 = cp.array(tupledataset2, dtype=cp.float32)
+dataset = TupleDataset(tupledataset1, tupledataset2)
 
-result = model(tupledataset1,tupledataset2)
-for i in range(10):
-	data = np.zeros((128, 128, 3), dtype=np.uint8)
-	dst = result.data[i] * 255.0
-	if uses_device >= 0:
-		dst = chainer.cuda.to_cpu(dst)
-	data[:,:,0] = dst[0]
-	data[:,:,1] = dst[1]
-	data[:,:,2] = dst[2]
-	himg = Image.fromarray(data, 'RGB')
-	himg.save('gen-'+str(i)+'.png')
-	
+# 繰り返し条件を作成する
+train_iter = iterators.SerialIterator(dataset, batch_size, shuffle=True)
+
+# 誤差逆伝播法アルゴリズムを選択する
+optimizer_gen = optimizers.Adam(alpha=0.0002, beta1=0.5)
+optimizer_gen.setup(model_gen)
+#model_gen.vgg16.disable_update()
+
+# デバイスを選択してTrainerを作成する
+updater = Updater(train_iter, {'opt_gen':optimizer_gen}, device=uses_device)
+trainer = training.Trainer(updater, (1000, 'epoch'), out="result")
+# 学習の進展を表示するようにする
+trainer.extend(extensions.ProgressBar())
+trainer.extend(extensions.LogReport(trigger=(500, 'epoch'), log_name='log'))
+trainer.extend(extensions.PlotReport(['loss'], x_key='epoch', file_name='loss.png'))
+
+# 中間結果を保存する
+n_save = 0
+@chainer.training.make_extension(trigger=(20, 'epoch'))
+def save_model(trainer):
+	# NNのデータを保存
+	global n_save
+	n_save = n_save+1
+	chainer.serializers.save_hdf5( 'gen-'+str(n_save)+'.hdf5', model_gen )
+trainer.extend(save_model)
+
+# 機械学習を実行する
+trainer.run()
+
+# 学習結果を保存する
+chainer.serializers.save_hdf5( 'gen.hdf5', model_gen )
